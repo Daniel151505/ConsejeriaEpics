4015ea7b122ddebe11ed727ed197931a6d8d5666
Daniel151505 <daniel150520011@gmail.com>
2021-01-17 18:11:49 -0500

    Agregando Metodo Usuario (Models)

---
+++ Models/Usuario.cs
@@ -0,0 +1,39 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ConsejeriaEpics.Models
+{
+
+    [Table("t_usuario")]
+    public class Usuario
+    {
+        [Required]
+        [Column("id")]
+        public int ID { get; set; }
+
+        [Required]
+        [Column("nombre")]
+        public string Nombre { get; set; }
+
+        [Required]
+        [Column("apellido")]
+        public string Apellido { get; set; }
+
+        [Required]
+        [Column("dni")]
+        public string DNI { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [Column("correo")]
+        public string Correo { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Column("password")]
+        public string Password { get; set; }
+
+        [Column("tipo")]
+        public string Tipo { get; set; }
+    }
+}
