c28aa9b4e3b04a4d983b8d0b8fa69a42b65d5647
Daniel151505 <daniel150520011@gmail.com>
2021-01-17 18:10:58 -0500

    Agregando Metodo Requerimiento (Models)

---
+++ Models/Requerimiento.cs
@@ -0,0 +1,41 @@
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ConsejeriaEpics.Models
+{
+    [Table("t_requerimiento")]
+    public class Requerimiento
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("id")]
+        public int ID { get; set; }
+
+        [Column("user_id")]
+        public int User_ID { get; set; }
+
+        [Required]
+        [Column("tipo_req")]
+        public int Tipo_Req { get; set; }
+
+        [Column("estado")]
+        public string Estado { get; set; }
+
+        [Required]
+        [Column("detalles")]
+        public string Detalle { get; set; }
+
+        [Column("fechaini")]
+        public DateTime Fecha_Inicio { get; set; }
+
+        [Column("fechafin")]
+        public DateTime? Fecha_Fin { get; set; }
+
+        [Column("consejero_id")]
+        public int? Consejero_ID { get; set; }
+
+        [Column("respuesta")]
+        public string Respuesta { get; set; }
+
+    }
+}
