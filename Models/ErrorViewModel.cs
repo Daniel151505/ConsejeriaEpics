3dc7dbfe7e56983432de1f596be128948bbf3ce7
Daniel151505 <daniel150520011@gmail.com>
2021-01-17 13:50:13 -0500

    Creando Estructura MVC

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ConsejeriaEpics.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
