bac995e93e27a4a5ba034c460d720aee67e5f67b
Daniel151505 <daniel150520011@gmail.com>
2021-01-17 18:50:28 -0500

    Agregando Estudiante Controller

---
+++ Controllers/EstudianteController.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ConsejeriaEpics.Models;
+using ConsejeriaEpics.Data;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Http;
+
+namespace ConsejeriaEpics.Controllers
+{
+    public class EstudianteController : Controller
+    {
+        private readonly ILogger<EstudianteController> _logger;
+        private readonly ApplicationDbContext _context;
+
+        public EstudianteController(ILogger<EstudianteController> logger, ApplicationDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var status=HttpContext.Session.GetString("State");          
+            if(status!=null){
+                var user = JsonConvert.DeserializeObject<Usuario>(HttpContext.Session.GetString("SessionUser"));
+                var tipo = user.Tipo;
+                if(tipo=="E"){
+                    return View();
+                }else{
+                    HttpContext.Session.Clear();
+                    return RedirectToAction("Index","Login");
+                }
+
+            }else{
+                return RedirectToAction("Index","Login");
+            }    
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
